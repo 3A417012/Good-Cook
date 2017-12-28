c7aa937ea1c39d556a0d9b6da2caf88c7a4add66
img21326 <img21326@gmail.com>
2017-12-28 22:20:56 +0800

    dechange photo to null

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,23 +16,21 @@
             </div>
             <div class="card-block">
                 <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
                 <div class="row card-text mt-3">
                     <div class="col-md-2">
                     </div>
                     <div class="col-md-4">
-                        <div class="pic col-md-8">
-                            <label for="userImage">
-                                <img id="userImage_img" data-src="holder.js/300x300" class="rounded mx-auto d-block" alt="300x300" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1609bc2ab9b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1609bc2ab9b%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.4296875%22%20y%3D%22104.5%22%3E300x300%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 300px; height: 300px;">
-                                <input name="userImage" id="userImage" type="file" class="inputFile"  style="display:none" onChange="showPreview(this);" />
-                            </label>
+                        {{--<div class="pic col-md-8">--}}
+                            {{--<label for="userImage">--}}
+                                {{--<img id="userImage_img" data-src="holder.js/300x300" class="rounded mx-auto d-block" alt="300x300" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1609bc2ab9b%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1609bc2ab9b%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.4296875%22%20y%3D%22104.5%22%3E300x300%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 300px; height: 300px;">--}}
+                                {{--<input name="userImage" id="userImage" type="file" class="inputFile"  style="display:none" onChange="showPreview(this);" />--}}
+                            {{--</label>--}}
 
-                        </div>
+                        {{--</div>--}}
                     </div>
                     <div class="col-md-6">
-                        <div class="form-horizontal align-items-center justify-content-center>
-
-                            {{ csrf_field() }}
-
+                        <div class="form-horizontal align-items-center justify-content-center">
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                 <label for="name" class="col-md-4 control-label">姓名</label>
 
