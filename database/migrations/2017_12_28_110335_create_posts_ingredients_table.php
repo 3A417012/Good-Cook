5193435192da5c63a69953440fa59f0c4827f0b8
img21326 <img21326@gmail.com>
2017-12-28 19:08:21 +0800

    create init database table

--- database/migrations/2017_12_28_110335_create_posts_ingredients_table.php
+++ database/migrations/2017_12_28_110335_create_posts_ingredients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsIngredientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts_ingredients', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('photo');
-            $table->rememberToken();
+            $table->integer("post_id")->index();
+            $table->integer("ingredients_id")->index();
+            $table->integer("amount");
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts_ingredients');
     }
 }
