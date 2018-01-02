f08db8a6372da10e3f02bf343bd699632e328f23
img21326 <img21326@gmail.com>
2018-01-02 14:07:21 +0800

    no message

--- config/imgur.php
+++ config/imgur.php
@@ -10,4 +10,5 @@ return [
     'client_secret' => env('IMGUR_CLIENT_SECRET', null),
 //    IMGUR_CLIENT_ID = d553166e71f376b
 //    IMGUR_CLIENT_SECRET = 58f30bac407cee616910e44df7132d74d335c574
+// debug curl.cainfo  https://laracasts.com/discuss/channels/general-discussion/curl-error-60-ssl-certificate-problem-unable-to-get-local-issuer-certificate/replies/37017
 ];
