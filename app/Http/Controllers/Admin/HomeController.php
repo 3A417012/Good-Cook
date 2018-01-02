9e0a02c0247e90d77a62c3b5d80e66235fa19b60
img21326 <img21326@gmail.com>
2018-01-02 11:09:01 +0800

    no message

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Ingredient;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -22,5 +23,14 @@ class HomeController extends Controller
 
     public function ingredientadd(){
         return view("admin.ingredient.edit");
+    }
+
+    public function ingredientstore(Request $request){
+        $a = new Ingredient();
+        $a->name = $request->name;
+        $a->unit = $request->bbb;
+        $a->heat = $request->heat;
+        $a->save();
+        return redirect()->to("home");
     }
 }
