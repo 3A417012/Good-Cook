eab3224594860d88e1d29ad4aa547a7f96f68689
img21326 <img21326@gmail.com>
2018-01-03 09:53:47 +0800

    add poster

--- resources/views/frontend/post.blade.php
+++ resources/views/frontend/post.blade.php
@@ -26,6 +26,7 @@
     <path d="M4 16 C1 12 2 6 7 4 12 2 15 6 16 8 17 6 21 2 26 4 31 6 31 12 28 16 25 20 16 28 16 28 16 28 7 20 4 16 Z" />
 
 </svg>收藏人數 : {{$post->collections()->count()}}
+                                    <span>分享者：{{$post->User->name}}</span>
                         </div>
 
                         <span class="float-right"><button type="button" class="btn like" onclick="location.href='{{route('like',$post->id)}}'">加入收藏</button></span></div>
